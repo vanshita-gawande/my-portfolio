0c7193385ca50c96654f574a4f42fa7c6dc93928
vanshita-gawande <vanshitapatil01@gmail.com>
2025-10-29 16:28:40 +0530

    change ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -47,7 +47,7 @@ function Home() {
             </a>
             <a
               href="/resume.pdf"
-              className="inline-flex items-center gap-2 px-6 py-3 bg-[#f44c22] text-white font-medium rounded-md hover:bg-[#ff916f] transition duration-300"
+              className="inline-flex items-center gap-2 px-6 py-3 bg-[#972f14] text-white font-medium rounded-md hover:bg-[#ff916f] transition duration-300"
             >
               <FontAwesomeIcon icon={faDownload} className="text-sm" />
               Resume
